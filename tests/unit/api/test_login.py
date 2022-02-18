b557232f3b71cd51c7457559e8eb6c1149f06775
Kevin Yang <ky200617@gmail.com>
2022-02-17 23:51:18 -0500

    fix this FAILED tests/unit/api/test_login.py::TestLogin::test_login - AttributeError: 'TestLogin' object has no attribute 'client' LATER

--- tests/unit/api/test_login.py
+++ tests/unit/api/test_login.py
@@ -15,30 +15,33 @@ import os
 ## UNCOMMENT BELOW CODE IF YOU WANT TO TEST LOGIN
 
 # # from api.v1.main import refresh
-# app = api.init_app()
-
-# client = TestClient(app)
-
-# id_token = os.getenv("GOOGLE_ID_TOKEN")
-# token = os.getenv("TOKEN")
-# refresh_token = os.getenv("REFRESH_TOKEN")
-
-# if id_token is None or token is None or refresh_token is None:
-#     new_token = googleAuth()
-#     os.environ["GOOGLE_ID_TOKEN"] = new_token
-#     id_token = new_token
-
-# if len(sys.argv) > 1:
-#     if sys.argv[1] == 'refresh':
-#         id_token = googleAuth()
-#         os.environ["GOOGLE_ID_TOKEN"] = id_token
 
 # UNCOMMENT ABOVE CODE IF YOU WANT TO TEST LOGIN
-@unittest.skip("Only test when you want to use test login")
+# @unittest.skip("Only test when you want to use test login")
 class TestLogin(unittest.TestCase):
+    def init(self, methodName="runTest"):
+        super().init(methodName)
+        app = api.init_app()
+
+        self.client = TestClient(app)
+
+        self.id_token = os.getenv("GOOGLE_ID_TOKEN")
+        self.token = os.getenv("TOKEN")
+        self.refresh_token = os.getenv("REFRESH_TOKEN")
+
+        if self.id_token is None or self.token is None or self.refresh_token is None:
+            new_token = googleAuth()
+            os.environ["GOOGLE_ID_TOKEN"] = new_token
+            self.id_token = new_token
+
+        if len(sys.argv) > 1:
+            if sys.argv[1] == 'refresh':
+                self.id_token = googleAuth()
+                os.environ["GOOGLE_ID_TOKEN"] = self.id_token
+
     def test_login(self):
-        response = client.post("v1/login/",
-            json={"token": id_token},
+        response = self.client.post("v1/login/",
+            json={"token": self.id_token},
         )
         assert response.status_code == 201
         os.environ["TOKEN"] = response.json()["token"]
@@ -49,10 +52,14 @@ class TestLogin(unittest.TestCase):
         login_response = self.test_login()
         
         refresh_token = login_response["refresh"]
-        response = client.post("v1/refresh/",
+        response = self.client.post("v1/refresh/",
             headers={"Authorization": f"Bearer {refresh_token}"},
         )
         assert response.status_code == 201
     
+    def runTest(self):
+        self.init()
+        self.test_login()
+        self.test_refresh()
 if __name__ == "__main__":
     unittest.main()
