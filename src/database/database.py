e23ddf20e0a0681de70128a2ee47c757048d46ae
Kevin Yang <ky200617@gmail.com>
2022-02-19 17:33:05 -0500

    changed ip to local host in sqlalchemy db url

--- src/database/database.py
+++ src/database/database.py
@@ -11,7 +11,7 @@ else:
     cfg = ConfigParser()
     cfg.read("config.default.ini")
 
-SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{cfg['PostgreSQL']['user']}:{cfg['PostgreSQL']['password']}@127.0.0.1:5432/{cfg['PostgreSQL']['name']}"
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://{cfg['PostgreSQL']['user']}:{cfg['PostgreSQL']['password']}@localhost:5432/{cfg['PostgreSQL']['name']}"
 print(SQLALCHEMY_DATABASE_URL)
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
