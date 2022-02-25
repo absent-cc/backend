175532355539c1af615e42aafeb6c735e100425a
Kevin Yang <ky200617@gmail.com>
2022-02-25 18:18:04 -0500

    updated src

--- tests/alembic/env.py
+++ tests/alembic/env.py
@@ -7,7 +7,6 @@ from alembic import context
 
 from src.dataTypes.models import Base
 
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
