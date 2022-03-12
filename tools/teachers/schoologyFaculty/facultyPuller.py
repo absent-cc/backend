09ab52e024e1e8daca1161570b977c68554f24ab
Roshan Karim <roshan@rkarim.xyz>
2022-03-11 20:04:07 -0500

    fin schoology faculty puller web scraper, now just need to save as csv

--- tools/teachers/schoologyFaculty/facultyPuller.py
+++ tools/teachers/schoologyFaculty/facultyPuller.py
@@ -1,9 +1,32 @@
 from bs4 import BeautifulSoup
 import requests
+from requests.structures import CaseInsensitiveDict
 
 BASE_URL = "https://schoology.newton.k12.ma.us/school/40673949/faculty"
 
-cookies = 
-page = requests.get(BASE_URL)
+cookies = {
+    "SESS57ef7b3f45b691c5d2133f3db8eadd7e": "cb78361e423892687e22796433dc4f75"
+    }
 
-print(page.text)
+def pullPage(n: int):
+    addOnQuery = f"?page={n}"
+    fullURL = BASE_URL + addOnQuery
+    page = requests.get(fullURL, cookies=cookies)
+    
+    soup = BeautifulSoup(page.content, "html.parser")
+    raw_HTML_teachers = soup.findAll(class_="faculty-name")
+    for entry in raw_HTML_teachers:
+        getNameFromHTMLEntry(entry)
+
+def getNameFromHTMLEntry(entry) -> (str, str): # First Last
+    a_tag = entry.find("a")
+    split1 = str(a_tag).split(">")[1]
+    split2 = split1.split("<")[0]
+
+    nameSpaceSplit = split2.split(" ")
+    first = nameSpaceSplit[0]
+    last = nameSpaceSplit[1]
+
+    print(first, last) 
+    return first, last
+
