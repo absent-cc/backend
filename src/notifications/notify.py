8df2088cb6439530b6af1c815559ca0821d03145
Kevin Yang <ky200617@gmail.com>
2022-03-17 23:05:17 -0400

    fixing eof issue

--- src/notifications/notify.py
+++ src/notifications/notify.py
@@ -1,13 +1,15 @@
 from src.database.database import SessionLocal
-from ..dataTypes import structs
+from ..dataTypes import structs, models
 import datetime
 from ..database import crud
 
+from typing import Optional, List, Tuple
 class Notify():
     def __init__(self, school: structs.SchoolName, date: datetime.date = datetime.date.today()):
         self.db = SessionLocal()
         self.school = school
         self.date = date
-        self.absences = crud.getAbsenceList(self.db, self.date, self.school)
+        self.absences: Optional[List[models.Absence]] = crud.getAbsenceList(self.db, self.date, self.school)
 
-    def notify(self):
+    def computeTotalClassesCancelled(self):
+        pass
