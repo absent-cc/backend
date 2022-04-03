dcf704076dae821bdf22317169474344be7594a6
Kevin Yang <ky200617@gmail.com>
2022-04-03 17:46:44 -0400

    clean up

--- src/api/v1/routers/info.py
+++ src/api/v1/routers/info.py
@@ -2,14 +2,14 @@ import datetime
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from src.dataTypes import structs
+from src.dataTypes import structs, schemas
 
 from ....api import accounts, utils
 from ....database import crud
 
 router = APIRouter(prefix="/info", tags=["Info"])
 
-@router.get("/schedule/", response_model=structs.SchoolDay, status_code=200)
+@router.get("/schedule/", response_model=schemas.SchoolDay, status_code=200)
 async def getSchedule(
     date: datetime.date = None,
     db: Session = Depends(accounts.getDBSession), # Initializes a DB. 
@@ -20,14 +20,14 @@ async def getSchedule(
     specialDayInDB = crud.getSpecialDay(db, date=date)
 
     if specialDayInDB != None:
-        return structs.SpecialDay(
+        return schemas.SpecialDay(
             date=date,
             name=specialDayInDB.name,
             schedule=specialDayInDB.schedule,
             note=specialDayInDB.note
         )
     
-    return structs.NormalDay(
+    return schemas.NormalDay(
         date=date,
         schedule=structs.SchoolBlocksOnDayWithTimes()[date.weekday()]
     )
