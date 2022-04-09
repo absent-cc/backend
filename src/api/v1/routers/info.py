1f8dffcfc792116e68be12c8a93b3745f274f082
Kevin Yang <ky200617@gmail.com>
2022-04-09 10:28:35 -0400

    created week peeker for info endpoint

--- src/api/v1/routers/info.py
+++ src/api/v1/routers/info.py
@@ -1,17 +1,20 @@
 import datetime
+from webbrowser import get
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from src.dataTypes import structs, schemas
+from ....utils.weekGen import weekDayGenerator
 from ....api import accounts
 from ....database import crud
 
 router = APIRouter(prefix="/info", tags=["Info"])
 
+from typing import Optional, List
 
-@router.get("/schedule/", response_model=schemas.SchoolDay, status_code=200)
-async def getSchedule(
+@router.get("/schedule/day", response_model=schemas.SchoolDay, status_code=200)
+def getSchedule(
     date: datetime.date = None,
     db: Session = Depends(accounts.getDBSession),  # Initializes a DB.
 ):
@@ -31,3 +34,21 @@ async def getSchedule(
     return schemas.NormalDay(
         date=date, schedule=structs.SchoolBlocksOnDayWithTimes()[date.weekday()]
     )
+
+@router.get("/schedule/week/", response_model=List[schemas.SchoolDay], status_code=200)
+def weekPeek(
+    date: Optional[datetime.date] = None,
+    db: Session = Depends(accounts.getDBSession),  # Initializes a DB.
+):
+    if date is None:
+        date = datetime.date.today()
+    
+    year, week, weekday = date.isocalendar()
+
+    if weekday is 5 or weekday is 6:
+        week += 1
+    
+    # Generate weekdays to iterate over:
+    weekdays: List[datetime.date] = weekDayGenerator(year, week)
+    
+    return [getSchedule(day, db) for day in weekdays]
